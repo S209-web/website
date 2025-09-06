1f6883d6a374d82f093afda4d5c0e94ec7a20881
S209-web <bhriguverma11@gmail.com>
2025-09-06 23:21:50 +0530

    made substantial changes in home page

--- src/components/brand/MarqueeAreaHomeOne.tsx
+++ src/components/brand/MarqueeAreaHomeOne.tsx
@@ -8,10 +8,30 @@ const MarqueeAreaHomeOne = () => {
       <div className="cs_moving_section_wrap cs_bold">
         <div className="cs_moving_section_in">
           <div className="cs_moving_section cs_stroke_text">
-            Let's Shout Out Of The Box
+            Let's Shout Out Of The Box <span style={{ 
+              color: '#F33C52', 
+              fontSize: '1.2em', 
+              margin: '0 20px',
+              display: 'inline-block'
+            }}>●</span> Let's Shout Out Of The Box <span style={{ 
+              color: '#F33C52', 
+              fontSize: '1.2em', 
+              margin: '0 20px',
+              display: 'inline-block'
+            }}>●</span>
           </div>
           <div className="cs_moving_section cs_stroke_text">
-            Let's Shout Out Of The Box
+            Let's Shout Out Of The Box <span style={{ 
+              color: '#F33C52', 
+              fontSize: '1.2em', 
+              margin: '0 20px',
+              display: 'inline-block'
+            }}>●</span> Let's Shout Out Of The Box <span style={{ 
+              color: '#F33C52', 
+              fontSize: '1.2em', 
+              margin: '0 20px',
+              display: 'inline-block'
+            }}>●</span>
           </div>
         </div>
       </div>
